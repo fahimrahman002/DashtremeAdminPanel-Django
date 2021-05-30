0d23d2f64ad1129393b911d92901e9bb2df363f2
Fahim Rahman <57598021+fahimrahman002@users.noreply.github.com>
2021-05-30 21:55:48 +0600

    included template components

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from app import views
 urlpatterns=[
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('home',views.home,name='home'),
+    path('calendar',views.calendar,name='calendar'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 
 def home(response):
     return render(response,"index.html")
+def calendar(response):
+    return render(response,"calendar.html")
